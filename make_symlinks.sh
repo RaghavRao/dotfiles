fd3fa33e00fb580acf4ca85c6f37bbcb749c15ed
Raghav Rao <raghav.rao32@gmail.com>
2018-10-09 09:55:11 -0400

    bind inputrc

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-ln -s "$HOME/.dotfiles/.bash_aliases" "$HOME/.bash_aliases"
-ln -s "$HOME/.dotfiles/.bash_functions" "$HOME/.bash_functions"
-ln -s "$HOME/.dotfiles/.bash_ps1" "$HOME/.bash_ps1"
-ln -s "$HOME/.dotfiles/.bashrc" "$HOME/.bashrc"
-ln -s "$HOME/.dotfiles/.bash_snippets" "$HOME/.bash_snippets"
-ln -s "$HOME/.dotfiles/.inputrc" "$HOME/.inputrc"
+ln -s ".bash_aliases" "$HOME/.bash_aliases"
+ln -s ".bash_functions" "$HOME/.bash_functions"
+ln -s "$.bash_ps1" "$HOME/.bash_ps1"
+ln -s "$.bashrc" "$HOME/.bashrc"
+ln -s "$.bash_snippets" "$HOME/.bash_snippets"
+ln -s "$.inputrc" "$HOME/.inputrc"
+ln -s "$.pythonrc" "$HOME/.pythonrc"
+echo "binding new .inputrc to current session..."
+bind -f "$HOME/.inputrc"
 
